1bcdb14f1a5a3e751f0410c8506f8eb9eb169946
Mattia Gheda <ghedamat@gmail.com>
2013-09-19 17:43:54 -0400

    cleanup config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,6 @@ set :environment, 'production'
 server 'emberlogs.ghedamat.com', :web, :app, :db, primary: true
 set :deploy_to, "/home/deploy/#{application}"
 
-
-
-#set :scm_user, 'ghedamat'
 set :user, 'deploy'
 set :copy_exclude, [".git"]
 
@@ -22,48 +19,9 @@ ssh_options[:forward_agent] = true
 set :rvm_type, :user
 set :rvm_ruby_string, 'default'
 
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-#role "emberlogs.ghedamat.com", :app, :web, :db, :primary => true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-#
-
-# env
-#set :bundle_flags, "--deployment --no-prune --quiet"
-
 # assets
 set :normalize_asset_timestamps, false
 
-#after "deploy:setup", :create_required_shared_dirs
-#task :create_required_shared_dirs, :roles => :app do
-#  run "mkdir -p #{shared_path}/config"
-#end
-
-#after "deploy:update_code", :link_shared_resources
-#task :link_shared_resources, :roles => :app do
-#  run "ln -nfs #{shared_path}/config/*.yml #{release_path}/config/"
-#end
 namespace :deploy do
 task :symlink_config, roles: :app do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
